fcd79a17cbae06de134d7f75906bb14aef70cc3a
Juan <zapjuanfra97@gmail.com>
2021-06-20 22:16:21 -0500

    Creacion clase empleado

---
+++ src/empresa/Empleado.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package empresa;
+
+/**
+ *
+ * @author zapju
+ */
+public class Empleado {
+    String cedula, nombre, apellido, direccion;
+
+    public Empleado(String cedula, String nombre, String apellido, String direccion) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.direccion = direccion;
+    }
+    
+}
