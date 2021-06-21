3bf7af7e733b44aee067672ea29041a79129987c
Jean Giron <jean_0720@hotmail.com>
2021-06-20 22:17:38 -0500

    Creacion de clase atencion

---
+++ src/empresa/Atencion.java
@@ -0,0 +1,17 @@
+
+package empresa;
+
+/**
+ *
+ * @author jean_
+ */
+public class Atencion {
+    int numAtencion;
+    String fechaAtencion;
+    Empleado empleadoAtencion;
+
+    public Atencion(int numAtencion, String fechaAtencion) {
+        this.numAtencion = numAtencion;
+        this.fechaAtencion = fechaAtencion;
+    }
+}
