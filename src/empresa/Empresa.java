3a159526fdc24886ef320f70ed6810396e634e53
Jean Giron <jean_0720@hotmail.com>
2021-06-20 22:11:28 -0500

    Creacion del proyecto

---
+++ src/empresa/Empresa.java
@@ -0,0 +1,17 @@
+
+package empresa;
+
+/**
+ *
+ * @author jean_
+ */
+public class Empresa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
