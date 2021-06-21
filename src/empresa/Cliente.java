16cfe296248acf05e622e226cd2004e76cfb9497
Fernando <gatoguevara2@hotmail.com>
2021-06-20 22:16:15 -0500

    Creacion clase Cliente

---
+++ src/empresa/Cliente.java
@@ -0,0 +1,14 @@
+
+package empresa;
+
+public class Cliente {
+    
+    private String nombre, apellido, ci;
+
+    public Cliente(String nombre, String apellido, String ci) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.ci = ci;
+    }
+
+}
